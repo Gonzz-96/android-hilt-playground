7ec0458a609f144449e77038e87b54ba7f7e109b
Gonzalo Campos <erick.campos@wizeline.com>
2020-07-24 22:28:55 -0500

    Create MainViewModel

---
+++ app/src/main/java/com/gonz/mx/hilt/playground/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.gonz.mx.hilt.playground
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class MainViewModel @ViewModelInject constructor() : ViewModel()
