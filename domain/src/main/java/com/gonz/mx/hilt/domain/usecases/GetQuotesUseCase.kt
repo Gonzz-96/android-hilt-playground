b8854cf93da439d40e0512b16277369501005f4d
Gonzalo Campos <erick.campos@wizeline.com>
2020-07-24 22:16:43 -0500

    Annotate use case with @Inject

--- domain/src/main/java/com/gonz/mx/hilt/domain/usecases/GetQuotesUseCase.kt
+++ domain/src/main/java/com/gonz/mx/hilt/domain/usecases/GetQuotesUseCase.kt
@@ -1,8 +1,9 @@
 package com.gonz.mx.hilt.domain.usecases
 
 import com.gonz.mx.hilt.domain.entities.Quote
+import javax.inject.Inject
 
-class GetQuotesUseCase {
+class GetQuotesUseCase @Inject constructor() {
     operator fun invoke(): List<Quote> {
         TODO("Not yet implemented")
     }
