bb6e6ce5414b89846b5cb6931c6a7ea281be252f
Gonzalo Campos <erick.campos@wizeline.com>
2020-07-25 16:32:34 -0500

    Inject local data source into QuotesRepository.

--- data/src/main/java/com/gonz/mx/hilt/data/repo/QuotesRepository.kt
+++ data/src/main/java/com/gonz/mx/hilt/data/repo/QuotesRepository.kt
@@ -1,12 +1,15 @@
 package com.gonz.mx.hilt.data.repo
 
+import com.gonz.mx.hilt.data.sources.QuotesLocalDataSource
 import com.gonz.mx.hilt.domain.entities.Quote
 import com.gonz.mx.hilt.domain.gateways.QuotesGateway
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class QuotesRepository @Inject constructor(): QuotesGateway {
+class QuotesRepository @Inject constructor(
+    private val quotesLocalDataSource: QuotesLocalDataSource
+): QuotesGateway {
     override fun getQuotes(): List<Quote> {
         TODO("Not yet implemented")
     }
