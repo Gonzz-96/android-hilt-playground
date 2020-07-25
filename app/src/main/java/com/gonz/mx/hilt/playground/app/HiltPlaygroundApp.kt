ae94258f588c6f630bc350af367e17f417f731a3
Gonzalo Campos <erick.campos@wizeline.com>
2020-07-24 21:42:14 -0500

    Arrange main activity UI.

--- app/src/main/java/com/gonz/mx/hilt/playground/app/HiltPlaygroundApp.kt
+++ app/src/main/java/com/gonz/mx/hilt/playground/app/HiltPlaygroundApp.kt
@@ -5,7 +5,7 @@ import dagger.hilt.android.HiltAndroidApp
 
 /**
  * Hilt provides a single annotation that will mark
- * our application class: @HildAndroidApp.
+ * our application class: @HiltAndroidApp.
  * This annotation will help us to reduce all the
  * Dagger's boilerplate, for example, the
  * HasAndroidInjector interface, and will start
