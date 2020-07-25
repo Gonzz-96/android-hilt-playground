acec9cd00909fb4fbce09b8e3770fa9a585bdad2
Gonzalo Campos <erick.campos@wizeline.com>
2020-07-24 20:53:02 -0500

    Initial commit.

---
+++ app/src/main/java/com/gonz/mx/hilt/playground/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gonz.mx.hilt.playground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
