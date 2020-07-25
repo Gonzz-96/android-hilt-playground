b4f5b07de14e5f09b806161d30370e21d1c09ada
Gonzalo Campos <erick.campos@wizeline.com>
2020-07-25 15:36:29 -0500

    Create data module.

--- domain/src/main/java/com/gonz/mx/hilt/domain/usecases/GetQuotesUseCase.kt
+++ domain/src/main/java/com/gonz/mx/hilt/domain/usecases/GetQuotesUseCase.kt
@@ -3,6 +3,15 @@ package com.gonz.mx.hilt.domain.usecases
 import com.gonz.mx.hilt.domain.entities.Quote
 import javax.inject.Inject
 
+/**
+ * In Hilt, there are several scopes that will correspond
+ * to different default components provided by it. When
+ * a class has no scope, meaning that has no scope annotations,
+ * a new instance will be created in every request.
+ *
+ * In the case of this use case, there will be a new instance
+ * of it every time a ViewModel request it.
+ */
 class GetQuotesUseCase @Inject constructor() {
     operator fun invoke(): List<Quote> {
         TODO("Not yet implemented")
