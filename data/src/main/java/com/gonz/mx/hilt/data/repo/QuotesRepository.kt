18063af5497e2461097cdfc86acd79dd26e2f224
Gonzalo Campos <erick.campos@wizeline.com>
2020-07-25 20:08:38 -0500

    Inject remote data source into QuotesRepository.

--- data/src/main/java/com/gonz/mx/hilt/data/repo/QuotesRepository.kt
+++ data/src/main/java/com/gonz/mx/hilt/data/repo/QuotesRepository.kt
@@ -1,6 +1,7 @@
 package com.gonz.mx.hilt.data.repo
 
 import com.gonz.mx.hilt.data.di.annotations.QuotesLocalDataSourceAnnotation
+import com.gonz.mx.hilt.data.di.annotations.QuotesRemoteDataSourceAnnotation
 import com.gonz.mx.hilt.data.sources.QuotesDataSource
 import com.gonz.mx.hilt.domain.entities.Quote
 import com.gonz.mx.hilt.domain.gateways.QuotesGateway
@@ -9,7 +10,8 @@ import javax.inject.Singleton
 
 @Singleton
 class QuotesRepository @Inject constructor(
-    @QuotesLocalDataSourceAnnotation private val quotesLocalDataSource: QuotesDataSource
+    @QuotesLocalDataSourceAnnotation private val quotesLocalDataSource: QuotesDataSource,
+    @QuotesRemoteDataSourceAnnotation private val quotesRemoteDataSource: QuotesDataSource
 ): QuotesGateway {
     override fun getQuotes(): List<Quote> {
         TODO("Not yet implemented")
